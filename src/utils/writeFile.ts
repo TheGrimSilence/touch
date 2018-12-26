3a07f0ddca43771114b420d23b151942f5e3e384
Adrian (Grim) Roach <silencegrim@gmail.com>
2018-12-25 22:48:49 -0500

    We can make files and folders! 🎉

--- src/utils/writeFile.ts
+++ src/utils/writeFile.ts
@@ -1,9 +1,25 @@
-import { writeFileSync } from 'fs';
+import { mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
-export function writeEmptyFile(files: string[]) {
+import { parseFile } from './parsePath';
+
+/**
+ * Create files with the given parameters.
+ * @param files The files to be created.
+ * @param contents The data to be written into the files
+ */
+export function writeFile(files: string[]) {
+  let data: string;
+
+  data = '';
+
   files.forEach((file) => {
-    const contents = '';
-    writeFileSync(join(__dirname, '..', 'temp', file), contents, 'utf8');
+    const fileInfo = parseFile(file);
+    mkdirSync(fileInfo.path, { recursive: true });
+    writeFileSync(
+      join(process.cwd(), fileInfo.path, fileInfo.base),
+      data,
+      'utf8',
+    );
   });
 }
--- src/xtouch.ts
+++ src/xtouch.ts
@@ -1,14 +1,8 @@
-import { writeEmptyFile } from 'utils/writeFile';
+import { writeFile } from 'utils/writeFile';
 
 // import * as chalk from 'chalk';
 const args = process.argv.slice(2);
 
 // TODO: Are we creating a file, running interactive, or creating a project?
 
-// ! TODO: Add support for paths!
-
-console.log(args);
-
-// console.log(chalk.default`{red Test}`);
-
-writeEmptyFile(args);
+writeFile(args);
