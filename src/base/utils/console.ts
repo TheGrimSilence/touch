0bf359858e154e6a70f7dd4fb1ce435ab2124f66
Adrian (Grim) Roach <silencegrim@gmail.com>
2018-12-27 18:45:21 -0500

    Remove string restrictions

--- src/base/utils/console.ts
+++ src/base/utils/console.ts
@@ -1,13 +1,13 @@
 import chalk from 'chalk';
 
-export function error(msg: string): void {
-  return console.error(chalk`[{red error}] ${msg}`);
+export function error(msg): void {
+  return console.error(chalk`[{red error}]`, msg);
 }
 
-export function info(msg: string): void {
-  return console.info(chalk`[{blue info}] ${msg}`);
+export function info(msg): void {
+  return console.info(chalk`[{blue info}]`, msg);
 }
 
-export function warn(msg: string): void {
-  return console.warn(chalk`[{red warn}] ${msg}`);
+export function warn(msg): void {
+  return console.warn(chalk`[{red warn}]`, msg);
 }
