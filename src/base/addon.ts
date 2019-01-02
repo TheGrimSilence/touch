305296eff7bb70650212c345a6ae118401e86473
Adrian (Grim) Roach <silencegrim@gmail.com>
2019-01-02 03:35:24 -0500

    Add regexes for upcoming features

--- src/base/addon.ts
+++ src/base/addon.ts
@@ -24,6 +24,33 @@ export function addon(options: IAddonOptions): void {
   const addonPath = join(path, options.addon, options.type);
   const addonData = readFileSync(addonPath, 'utf8');
   const filename = /(?<=\/\/ @xt-filename(\s|=|:)).*$/m.exec(addonData)[0];
+  const script = /(?:\/\/ @xt-script(?:\s|:|=)([a-zA-Z]*)(?:\s|:|=)"(.*)")/m.exec(
+    addonData,
+  )[0];
+  const scriptName = /(?:\/\/ @xt-script-name(?:\s|:|=)([a-zA-Z]*))/m.exec(
+    addonData,
+  )[0];
+  const scriptCommand = /(?:\/\/ @xt-script-command(?:\s|:|=)"(.*)")/m.exec(
+    addonData,
+  )[0];
+  const dependency = /(?:\/\/ @xt-dependency(?:\s|:|=)((?:@[a-z]*\/[a-z]*)|(?:[a-z]*))@((?:~?[0-9]+\.[0-9]+\.[0-9]+)|([a-z]*)))/m.exec(
+    addonData,
+  )[0];
+  const dependencyName = /(?:\/\/ @xt-dependency-name(?:\s|:|=)((?:@[a-z]*\/[a-z]*)|(?:[a-z]*)))/m.exec(
+    addonData,
+  )[0];
+  const dependencyVersion = /(?:\/\/ @xt-dependency-version(?:\s|:|=)((?:~?[0-9]+\.[0-9]+\.[0-9]+)|(?:[a-z]*)))/m.exec(
+    addonData,
+  )[0];
+  const devDependency = /(?:\/\/ @xt-devDependency(?:\s|:|=)((?:@[a-z]*\/[a-z]*)|(?:[a-z]*))@((?:~?[0-9]+\.[0-9]+\.[0-9]+)|([a-z]*)))/m.exec(
+    addonData,
+  )[0];
+  const devDependencyName = /(?:\/\/ @xt-devDependency-name(?:\s|:|=)((?:@[a-z]*\/[a-z]*)|(?:[a-z]*)))/m.exec(
+    addonData,
+  )[0];
+  const devDependencyVersion = /(?:\/\/ @xt-devDependency-version(?:\s|:|=)((?:~?[0-9]+\.[0-9]+\.[0-9]+)|(?:[a-z]*)))/m.exec(
+    addonData,
+  )[0];
   const data = addonData
     .split('\n')
     .slice(1)
