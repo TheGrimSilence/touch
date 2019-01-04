237813efe33bde1ccd04c42763c38494976ef97a
Adrian (Grim) Roach <silencegrim@gmail.com>
2019-01-03 23:09:09 -0500

    Introduce verbose-locked logging

--- src/base/utils/console.ts
+++ src/base/utils/console.ts
@@ -1,13 +1,55 @@
 import chalk from 'chalk';
 
-export function error(msg, ...rest): void {
-  return console.error(chalk`[{red error}]`, msg, rest);
+/** Returns a red verbose-locked message. */
+export function errorVerbose(msg: string): void {
+  if (verbose) {
+    return console.error(chalk`[{red error}]`, msg);
+  } else {
+    return;
+  }
 }
 
-export function info(msg, ...rest): void {
-  return console.info(chalk`[{blue info}]`, msg, rest);
+/** Returns a blue verbose-locked message. */
+export function infoVerbose(msg: string | string[] | {}): void {
+  if (verbose) {
+    return console.info(chalk`[{blue info}]`, msg);
+  } else {
+    return;
+  }
 }
 
-export function warn(msg, ...rest): void {
-  return console.warn(chalk`[{red warn}]`, msg, rest);
+/** Returns a yellow verbose-locked message. */
+export function warnVerbose(msg: string): void {
+  if (verbose) {
+    return console.warn(chalk`[{yellow warn}]`, msg);
+  } else {
+    return;
+  }
+}
+
+/** Returns a red message. */
+export function error(msg: string): void {
+  if (verbose) {
+    return console.error(chalk`[{red error}]`, msg);
+  } else {
+    return;
+  }
+}
+
+/** Returns a blue message. */
+export function info(msg: string | string[] | {}): void {
+  if (verbose) {
+    return console.info(chalk`[{blue info}]`, msg);
+  } else {
+    return;
+  }
+}
+
+/** Returns a yellow message. */
+export function warn(msg: string): void {
+  if (verbose) {
+    return console.warn(chalk`[{yellow warn}]`, msg);
+  } else {
+    return;
+  }
 }
